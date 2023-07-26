3b733d92c636834501cad96337b1032bce54d89b
Teetoomaa <tekimoraa@gmail.com>
2023-07-26 16:16:31 +0100

    Signup Correction

--- source/Pages/Signup.tsx
+++ source/Pages/Signup.tsx
@@ -85,7 +85,7 @@ export default function Signup() {
           <div className="flex items-start my-6">
             <div className="flex items-center h-5">
               <input
-                type="terms"
+                id="terms"
                 aria-describedby="terms"
                 type="checkbox"
                 className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
