8d9ed2db0d3fc9e749b82da1315b230ea854220f
Teetoomaa <tekimoraa@gmail.com>
2023-08-02 12:53:35 +0100

    Code Update

--- source/Components/Header.tsx
+++ source/Components/Header.tsx
@@ -22,38 +22,41 @@ export const MenuItems = ({ name, url }: { name: string; url: string }) => {
 };
 
 export const Header = () => {
-
   return (
     <div className="bg-white h-20">
       <div className="flex w-4/5 mx-auto justify-between items-center h-full">
         <div className="w-full">
           <Image src="/assets/logo.png" width={164} height={32} alt="Logo" />
         </div>
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-8">
           <MenuItems name="Home" url="/" />
-          <MenuItems name="About" url="/about" />
           <MenuItems name="Candidates" url="/candidates" />
           <a
             href="/login"
-            className="flex items-center justify-center h-20 px-5 text-sm font-medium text-center 
-            text-white border rounded-lg xl:h-10 bg-[#d8a642]"
+            className="flex items-center justify-center h-20 px-5 text-sm 
+          text-center text-black transition-colors duration-200 transform border rounded-lg xl:h-10 
+          border-black focus:outline-none hover:border-[#d8a642] hover:bg-[#d8a642] hover:text-white"
           >
             Login
           </a>
           <a
             href="/signup"
             className="flex items-center justify-center h-20 px-5 text-sm 
-          text-center text-[#d8a642] transition-colors duration-200 transform border rounded-lg xl:h-10 
-          text-[#d8a642] border-[#d8a642] focus:outline-none"
+          text-center text-black transition-colors duration-200 transform border rounded-lg xl:h-10 
+          border-black focus:outline-none hover:border-[#d8a642] hover:bg-[#d8a642] hover:text-white"
           >
             Signup
+          </a>
+          <a
+            href="/apply"
+            className="flex items-center justify-center h-20 px-5 text-sm 
+          text-center text-black transition-colors duration-200 transform border rounded-lg xl:h-10 
+          border-black focus:outline-none hover:border-[#d8a642] hover:bg-[#d8a642] hover:text-white"
+          >
+            Apply
           </a>
         </div>
       </div>
     </div>
-
-    
   );
 };
-
-
